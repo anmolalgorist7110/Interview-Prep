c7def623c1c1088825c6dc513c1d1cc16d398c5c
Anmol Raj Singh <77536919+anmolalgorist7110@users.noreply.github.com>
2023-10-06 01:07:19 +0530

    Time: 118 ms (24.42%), Space: 17.7 MB (49.52%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in set(nums):
+            if nums.count(i) > len(nums) // 3:
+                res.append(i)
+        return res
