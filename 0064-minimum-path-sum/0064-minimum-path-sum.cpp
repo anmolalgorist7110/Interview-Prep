03aaf8c3a67462c4136e84d4b949b8b422e2a28f
Anmol Raj Singh <77536919+anmolalgorist7110@users.noreply.github.com>
2023-10-15 12:44:02 +0530

    Time: 7 ms (72.13%), Space: 10.3 MB (27.11%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
 
-   int helper(int i , int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
-       // base case 1
-       if(i == 0 and j == 0)
-       return grid[0][0] ;
+   int helper(int n , int m , vector<vector<int>>& grid ){
+    vector<vector<int>> dp(n , vector<int>(m , 0)) ;
 
-       // base case 2
-       if(i<0 or j < 0)
-       return 1e9  ;
+    for(int i=0 ; i<n ;i++){
+        for(int j=0  ; j<m ; j++){
+            if(i== 0 and j== 0)
+            dp[i][j] = grid[i][j] ;
+            else{
+                int up = grid[i][j] ;
 
-       if(dp[i][j] != -1)
-       return dp[i][j] ;
+                if(i>0)
+                up += dp[i-1][j] ;
+                else up += 1e9 ;
+
+                int left = grid[i][j] ;
+
+                if(j>0)
+                left += dp[i][j-1] ;
+                else left += 1e9 ;
 
 
+                dp[i][j] = min(up , left) ;
+            }
+        }
+    }
 
-       int up = grid[i][j] + helper(i-1 , j , grid , dp) ;
-       int left = grid[i][j] + helper(i , j-1 , grid , dp) ;
-
-
-       return dp[i][j] = min(up , left) ;
+    return dp[n-1][m-1] ;
    }
 
 
@@ -27,9 +35,7 @@ public:
         int n = grid.size() ;
         int m = grid[0].size() ;
 
-        vector<vector<int>> dp(n , vector<int>(m , -1)) ;
-
-        return helper(n-1 , m-1 , grid , dp) ;
+        return helper(n , m , grid ) ;
     }
     
 };
