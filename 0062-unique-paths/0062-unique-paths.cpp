3e8fec2640ca804cc71d0b196b35b95847f0512b
Anmol Raj Singh <77536919+anmolalgorist7110@users.noreply.github.com>
2023-10-15 11:26:09 +0530

    Time: 3 ms (27.93%), Space: 6.8 MB (8.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,40 @@
-
 class Solution {
 public:
+
+   int helper(int m , int n , vector<vector<int>>& dp){
+      // loop throug the grid using twonested loops
+
+      for(int i=0 ; i<m ; i++){
+          for(int j = 0 ; j<n ;j++){
+              // base case 
+              if(i==0 and j==0){
+                  dp[i][j] = 1 ;
+                  continue ;
+              }
+
+
+              int up = 0 ;
+              int left = 0 ;
+
+              if(i>0)
+              up = dp[i-1][j] ;
+
+              if(j>0)
+              left = dp[i][j-1] ;
+
+              dp[i][j] = up + left ;
+          }
+      }
+
+      return dp[m-1][n-1] ;
+   }
+
+
     int uniquePaths(int m, int n) {
-       vector<int> dp(n,1) ;
-       
-       
-       for(int i=1 ; i<m ; i++)
-       {
-       	for(int j=1 ; j<n ; j++)
-       	{
-       		dp[j] += dp[j-1] ;
-		   }
-	   }
-	   
-	   return dp[n-1] ;
+       // create a memo table (dp) to store the results of subproblems
+       vector<vector<int>> dp(m , vector<int>(n , -1)) ;
+
+       // call the utilty function with the bottom-right cell
+       return helper(m , n , dp) ;
     }
 };
