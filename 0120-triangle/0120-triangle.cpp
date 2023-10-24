97530916a82a11abf3757304516d326ea7b39030
Anmol Raj Singh <77536919+anmolalgorist7110@users.noreply.github.com>
2023-10-25 01:33:14 +0530

    Time: 4 ms (75.8%), Space: 9.3 MB (37.64%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
 
-    int helper(int i , int j , vector<vector<int>>& tri , int n , vector<vector<int>>& dp){
+    int helper(vector<vector<int>>& tri , int n){
+        vector<vector<int>> dp(n , vector<int>(n , 0)) ;
 
-        // if already cal
-        if(dp[i][j] != -1){
-            return dp[i][j] ;
+
+        //init the bottom row of the dp with the values from tri
+        for( int j=0 ; j<n ; j++){
+            dp[n-1][j] = tri[n-1][j] ;
         }
 
-        // if we are at bottom row  , return the value of the curr cell
-        if(i == n-1)
-        return tri[i][j] ;
 
+        //iterate through the tri rows in reverse order
 
-        int down = tri[i][j] + helper(i+1 , j , tri , n , dp) ;
-        int dg = tri[i][j] + helper(i+1 , j+1 , tri , n , dp) ;
+        for(int i = n-2 ; i>=0 ; i--){
+            for(int j=i ; j>=0 ; j--){
+          
+            // calculate the min path sum for the current cell
+            int down = tri[i][j] + dp[i+1][j] ;
+            int dg = tri[i][j] + dp[i+1][j+1] ;
 
+            //store the min of the two possible path in dp
+            dp[i][j] = min(down ,dg) ;
 
-        //store the min if the both
-        return dp[i][j] = min(down , dg) ;
+            }
+        }
+
+        return dp[0][0] ;
     }
 
-
-    int minimumTotal(vector<vector<int>>& tri){
+    int minimumTotal(vector<vector<int>>& tri) {
         int n = tri.size() ;
-        vector<vector<int>> dp(n , vector<int>(n  , -1));
-        return helper(0 , 0 , tri , n , dp) ;
+        return helper(tri  , n) ;
     }
 };
